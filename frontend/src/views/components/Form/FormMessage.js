204876d0c0f22be7c30c31192e77d120e580ffaf
shineunji <dmswlckato@gmail.com>
2020-10-19 15:07:38 +0900

    ValidationType Check

--- frontend/src/views/components/Form/FormMessage.js
+++ frontend/src/views/components/Form/FormMessage.js
@@ -4,14 +4,28 @@ import {ValidationTypes} from "../../../lib/Validate";
 import {pxToRem} from "../../../common/Text/Text.Styled";
 import {Color} from "../../../common/Color/Color.Styled";
 
-function FormMessage (props) {
+function FormMessage(props) {
 
     const {
-        error
+        name,
+        error,
     } = props;
 
     return (
         <Container>
+            {
+                name === 'nickname' &&
+                <span className={'default'}>한글과 영문을 포함한 15자까지 가능합니다. (특수기호 사용 불가)</span>
+            }
+            {
+                name === 'password' &&
+                <span className={'default'}>8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.</span>
+            }
+
+            {
+                error?.type &&
+                <span className={'error'}>필수정보입니다</span>
+            }
             {
                 error?.type === ValidationTypes.IS_EMAIL &&
                 <span>이메일 양식이 올바르지 않습니다.</span>
@@ -20,18 +34,39 @@ function FormMessage (props) {
                 error?.type === ValidationTypes.IS_VALID_NICKNAME &&
                 <span>한글과 영문을 포함한 15자까지 가능합니다. (특수기호 사용 불가)</span>
             }
+            {
+                error?.type === ValidationTypes.IS_VALID_PASSWORD &&
+                <span>8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.</span>
+            }
+            {
+                error?.type === ValidationTypes.REPEAT_PASSWORD &&
+                <span>8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.</span>
+            }
+
+            {
+
+            }
 
         </Container>
     )
 }
 
 const Container = styled.div`
-span {
-        position: relative;
-        top: ${pxToRem(-14)};
+    position:relative;
+    span {
+        position: absolute;
+        top: ${pxToRem(-25)};
         font-size: ${pxToRem(11)};
         font-weight: 300;
+        z-index: 10;
+        
+    }
+    .default {
+        color: #666;
+    }
+    .error {
         color: ${Color.RED};
     }
+}
 `
 export default FormMessage;
