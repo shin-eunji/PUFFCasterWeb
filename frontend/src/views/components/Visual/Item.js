abd2df3f5b14518af0d581464d5564b8262c9030
shineunji <dmswlckato@gmail.com>
2020-10-21 12:20:52 +0900

    tablet

--- frontend/src/views/components/Visual/Item.js
+++ frontend/src/views/components/Visual/Item.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import {ContentContainer} from "../../../common/Layout/Components.Styled";
+import {ContentContainer, customMedia} from "../../../common/Layout/Components.Styled";
 import {Button} from "../../../common/Button/Button.Styled";
 import {Images} from "../../../common/Images";
 import {pxToRem} from "../../../common/Text/Text.Styled";
@@ -77,6 +77,12 @@ const SContentContainer = styled(ContentContainer)`
     white-space: pre-wrap;
     padding-top: ${pxToRem(125)};
     
+    
+    ${customMedia.lessThan('tablet')`
+        display:flex;
+        flex-direction:column;
+        width: 100%;
+    `}
     &::before {
         content: '';
         position: absolute;
@@ -95,10 +101,21 @@ const SContentContainer = styled(ContentContainer)`
         width: ${pxToRem(470)};
         height: ${pxToRem(600)};
         background: url(${Images.parttern2});
+        ${customMedia.lessThan('tablet')`
+            right: ${pxToRem(30)};    
+        `}
     }
 `;
 const Text = styled.div`
     flex: 1;
+    ${customMedia.lessThan('tablet')`
+        width: 100%;
+        display:flex;
+        flex-direction:column;
+        align-items:center;
+        justify-content:center;
+        text-align:center;
+    `}
     h2 {
         color: ${Color.WHITE};
         font-size: ${pxToRem(48)};
@@ -162,10 +179,15 @@ const PlatformIcon = styled.div`
     justify-content: flex-start;
     img {
         margin-right: ${pxToRem(36)};
+        ${customMedia.lessThan('tablet')`
+            margin: 0 ${pxToRem(18)};
+        `}
     }
 `;
 const Thumbnail = styled.div`
-    
+    ${customMedia.lessThan('tablet')`
+        display:none;
+    `}
 `;
 
 export default Item;
